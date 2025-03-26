6ada52cb7570abf99466cfa526c1c23df3e7ffb3
Akinola Olusoga <olusogaakinola@gmail.com>
2025-03-26 03:52:13 +0100

    Update images url and Added JavaScript

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 "use strict";
 
-// INSTRUCTIONS
-/**
- * PIG GAME RULES:
- *
- * -
- */
+var instructions, scores, roundScore, activePlayer, dieDOM, gamePlaying;
 
-var scores, roundScore, activePlayer, diceDOM, gamePlaying;
+instructions = `
+PIG GAME RULES:
+====================
+1. The game has 2(two) players, playing in rounds
+
+2. In each turn, a player rolls the "DIE" as many times as he/she wishes to generate random score points. 
+
+3. Each result of each roll gets added to the current player's "ROUND SCORE"
+
+4. But if any player rolls a "1", the player losses all his current "ROUND SCORE" points which will not be added to his/her "GLOBAL SCORE". After that happens, it's the next player's turn
+
+5. The players can choose to "HOLD CURRENT VALUE", which means that his/her current "ROUND SCORE" points gets added to his "GLOBAL SCORE". After that happens, it's the next player's turn
+
+6. The first player to reach 50 points on "GLOBAL SCORE" wins the game
+
+TEST YOUR LUCK AGAINST EACH OTHER!!👌👍
+`;
 
 init();
+alert(instructions);
 
 // BUTTON NEW GAME
 document.querySelector(`.btn--new`).addEventListener(`click`, init);
 
-// BUTTON ROLL DICE
+// BUTTON ROLL die
 document.querySelector(`.btn--roll`).addEventListener(`click`, function () {
   if (gamePlaying) {
     // 1. Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var die = Math.floor(Math.random() * 6) + 1;
     // 2. Display result
-    var diceDOM = document.querySelector(`.dice`);
-    diceDOM.style.display = "block";
-    diceDOM.src = `dice-${dice}.png`;
+    var dieDOM = document.querySelector(`.die`);
+    dieDOM.style.display = "block";
+    dieDOM.src = `images/die-${die}.png`;
 
-    // 3. Update ROUND SCORE only if the rolled dice value is NOT `1`
-    if (dice !== 1) {
+    // 3. Update ROUND SCORE only if the rolled die value is NOT `1`
+    if (die !== 1) {
       // Add score
-      roundScore += dice;
+      roundScore += die;
       document.getElementById(`current--${activePlayer}`).textContent =
         roundScore;
     } else {
+      alert(`You rolled a "1", it is the next palyer's turn`);
       // Next player
       nextPlayer();
     }
@@ -46,10 +59,11 @@ document.querySelector(".btn--hold").addEventListener("click", function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // check if player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 50) {
       // refactor
-      document.getElementById(`name--${activePlayer}`).textContent = "WINNER!";
-      document.querySelector(`.dice`).style.display = "none";
+      alert(`Congrats, Player ${activePlayer + 1} won the game`);
+      document.getElementById(`name--${activePlayer}`).textContent = "WINNER!!";
+      document.querySelector(`.die`).style.display = "none";
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add("player--winner");
@@ -70,9 +84,9 @@ function init() {
   activePlayer = 0;
   gamePlaying = true;
 
-  diceDOM = document.querySelector(`.dice`);
+  dieDOM = document.querySelector(`.die`);
 
-  diceDOM.style.display = "none";
+  dieDOM.style.display = "none";
 
   document.getElementById(`score--0`).textContent = 0;
   document.getElementById(`current--0`).textContent = 0;
@@ -86,6 +100,7 @@ function init() {
   document.querySelector(`.player--1`).classList.remove("player--active");
 
   document.querySelector(`.player--0`).classList.add("player--active");
+  // alert(`You started a new game`);
 }
 
 // NEXT PLAYER
@@ -99,5 +114,5 @@ function nextPlayer() {
   document.querySelector(`.player--0`).classList.toggle("player--active");
   document.querySelector(`.player--1`).classList.toggle("player--active");
 
-  diceDOM.style.display = "none";
+  dieDOM.style.display = "none";
 }
